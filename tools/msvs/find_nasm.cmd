88a2698719720035b0ef064db473a69eead03b5c
johnnkp <johnnkp@hotmail.com>
2022-12-23 12:06:46 +0800

    Node.js v18.0.0a: rebase

---
+++ tools/msvs/find_nasm.cmd
@@ -0,0 +1,29 @@
+@IF NOT DEFINED DEBUG_HELPER @ECHO OFF
+
+ECHO Looking for NASM
+
+FOR /F "delims=" %%a IN ('where nasm 2^> NUL') DO (
+  EXIT /B 0
+)
+
+IF EXIST "%ProgramFiles%\NASM\nasm.exe" (
+  SET "Path=%Path%;%ProgramFiles%\NASM"
+  EXIT /B 0
+)
+
+IF EXIST "%ProgramFiles(x86)%\NASM\nasm.exe" (
+  SET "Path=%Path%;%ProgramFiles(x86)%\NASM"
+  EXIT /B 0
+)
+
+if EXIST "%LOCALAPPDATA%\bin\NASM\nasm.exe" (
+  SET "Path=%Path%;%LOCALAPPDATA%\bin\NASM"
+  EXIT /B 0
+)
+
+IF EXIST "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.34.31933\bin\Hostx64\x64\ml64.exe" (
+  SET "Path=%Path%;%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.34.31933\bin\Hostx64\x64"
+  EXIT /B 0
+)
+
+EXIT /B 1
